aa3bca3765516c63eb884d7f7f319c3f77c416f7
Tirta Styadi <tirta.setiady@yahoo.com>
2023-07-02 16:23:21 +1000

    Adding images component folder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,23 @@
 import "./NavbarStyles.css";
 
-import React from 'react'
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+
+    const [click , setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+     
+
+
   return (
     <div className="header">
         <Link to="/">
             <h1 id="headerLogo">RYJ</h1>
         </Link>
         {/* Sub Menu in Homepage */}
-        <ul className="nav-menu">
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li>
                 <Link to="/">Home</Link>
             </li>
@@ -24,6 +31,16 @@ const Navbar = () => {
                 <Link to="/Contact">Contact</Link>
             </li>
         </ul>
+
+        {/* Hamburger Menu */}
+        <div className="hamburger" onClick={handleClick}> 
+            { click ? (
+            <FaTimes size={20} style={{ color: "#fff" }}/>
+            ) : (
+            <FaBars size={20} style={{ color: "#fff" }}/>
+            )}
+        </div>
+
 
     </div>
   )
