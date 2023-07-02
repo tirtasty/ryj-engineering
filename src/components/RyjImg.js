80f6db4892c4be690e7237d579be9dd4b437defc
Tirta Styadi <tirta.setiady@yahoo.com>
2023-07-02 17:17:34 +1000

    Adding background and adjust homepage

--- src/components/RyjImg.js
+++ src/components/RyjImg.js
@@ -1,1 +1,31 @@
-import "./RyjImgStyles.css"
+import "./RyjImgStyles.css";
+
+import React from 'react'
+import backgroundImg from "../assets/background.jpg";
+import { Link } from "react-router-dom";
+
+
+
+const RyjImg = () => {
+  return (
+    <div className="ryj">
+        <div className="mask">
+            <img className="intro-img" src={backgroundImg} alt="backgroundImage" />
+        </div>
+        <div className="content">
+            <p>RYJ Engineering</p>
+            <h1 id="introWords" >AIR CONDITIONING | VENTILATION SOLUTIONS</h1>
+            <h2>Sydney, NSW</h2>
+            <div>
+                <Link to="/project" className="btn"></Link>
+                <Link to="/contact" className="btn btn-light"></Link>
+            </div>
+
+
+        </div>
+
+    </div>
+  )
+}
+
+export default RyjImg;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
+import RyjImg from '../components/RyjImg';
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <RyjImg />
         
     </div>
   )
