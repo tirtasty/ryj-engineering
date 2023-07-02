be6261698d4fd57fd427366f87116869c2063f10
Tirta Styadi <tirta.setiady@yahoo.com>
2023-07-03 08:51:08 +1000

    Adding footer Section

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import "./Footer.css";
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="footer-container">
+            <div className="left">
+                <div className="location">
+                    <FaHome />
+                </div>
+
+            </div>
+
+            <div className="right"></div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,11 +9,22 @@ const Navbar = () => {
 
     const [click , setClick] = useState(false);
     const handleClick = () => setClick(!click);
+
+    const [color, setColor] = useState(false);
+    const changeColor = () => {
+        if (window.scrollY >= 1) {
+            setColor(true);
+        } else {
+            setColor(false);
+        }
+    }
+
+    window.addEventListener("scroll", changeColor);
      
 
 
   return (
-    <div className="header">
+    <div className={color ? "header header-bg" : "header"}>
         <Link className="subHeader" to="/">
             <img className="logo-Image" src={logoImg} alt="logoImage" />
             <h1 className="headerLogo">RYJ</h1>
