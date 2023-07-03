ef7823df73f8fbc382ac37deed144861a6c5ec44
Tirta Styadi <tirta.setiady@yahoo.com>
2023-07-03 15:09:03 +1000

    Finishing Service Menu

--- src/routes/Service.js
+++ src/routes/Service.js
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Copyright from '../components/Copyright';
 import RyjImg2 from '../components/RyjImg2';
+import Services from '../components/ServiceList';
 
 
 const Service = () => {
@@ -11,6 +12,7 @@ const Service = () => {
     <div>
       <Navbar />
       <RyjImg2 heading="SERVICES" text="Air Conditioning & Ventilation solution"/>
+      <Services/>
       <Footer />
       <Copyright />
     </div>
