fdd822cd5812110a965c1710ac2bbcfae1d39f80
Tirta Styadi <tirta.setiady@yahoo.com>
2023-07-04 15:23:00 +1000

    Finishing up Contact Us Menu

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -4,7 +4,22 @@ import React from 'react'
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <div className="form">
+        <label>Your Name</label>
+        <input type="text" ></input>
+
+        <label>Email</label>
+        <input type="email"></input>
+
+        <label>Subject</label>
+        <input type="text"></input>
+
+        <label>Message</label>
+        <textarea row="6" placeholder="Type Here"/>
+
+        <button className="btn">Submit</button>
+
+    </div>
   )
 }
 
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -3,12 +3,16 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Copyright from '../components/Copyright';
+import ContactUs from '../components/ContactUs';
+import RyjImg2 from '../components/RyjImg2';
 
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
+      <RyjImg2 heading="Contact Us"/>
+      <ContactUs />
       <Footer />
       <Copyright />
     </div>
